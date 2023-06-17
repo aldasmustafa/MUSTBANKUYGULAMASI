ab744664c451237f0cc2cb75566dcfa144a2acab
aldasmustafa <129630250+aldasmustafa@users.noreply.github.com>
2023-06-17 17:45:59 +0300

    müşteri listele güncellendi

--- MUSTBANKUYGULAMASI/müşteri listele.cs
+++ MUSTBANKUYGULAMASI/müşteri listele.cs
@@ -36,10 +36,12 @@ namespace MUSTBANKUYGULAMASI
                 musteriler_table.Columns.Add("tc", typeof(string));
                 musteriler_table.Columns.Add("isim", typeof(string));
                 musteriler_table.Columns.Add("soyad", typeof(string));
+                musteriler_table.Columns.Add("telefon numarası", typeof(string));
+                musteriler_table.Columns.Add("adres", typeof(string));
 
                 foreach (FirebaseObject<musteri> musteri in musteriler)
                 {
-               musteriler_table.Rows.Add(musteri.Key, musteri.Object.isim, musteri.Object.soyad);
+               musteriler_table.Rows.Add(musteri.Key, musteri.Object.isim, musteri.Object.soyad,musteri.Object.telefonno, musteri.Object.adres);
                 }
 
                 musterilerdtg.DataSource = musteriler_table;
